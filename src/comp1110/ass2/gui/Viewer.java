3c79be7fcedbb6815234e9201c757b192155ec85
u6120911 <u6120911@anu.edu.au>
2019-05-02 12:52:35 +1000

    Add clear button

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -9,10 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import comp1110.ass2.RailroadInk;
@@ -26,7 +23,7 @@ import comp1110.ass2.RailroadInk;
 public class Viewer extends Application {
     /* board layout */
     private static final int VIEWER_WIDTH = 1024;
-    private static final int VIEWER_HEIGHT = 768;
+    private static final int VIEWER_HEIGHT = 640;
 
     private static final String URI_BASE = "assets/";
 
@@ -41,21 +38,19 @@ public class Viewer extends Application {
     //draw a grid
     private void displayGrid() {
         this.grid = new GridPane();
-        this.grid.setPrefSize(722,650); // (560+162, 560+90)
-        placementGroup.getChildren().add(grid);
+        this.grid.setPrefSize(450,450);
+        this.grid.setLayoutX(150);
+        this.grid.setLayoutY(100);
+        root.getChildren().add(grid);
 
         grid.setGridLinesVisible(true);
-        for(int i=0; i<8; i++){
+        for(int i=0; i<7; i++){
             RowConstraints row;
             ColumnConstraints col;
 
-            if(i==0){
-                row = new RowConstraints(90);
-                col = new ColumnConstraints(162);
-            }else {
-                row = new RowConstraints(80);
-                col = new ColumnConstraints(80);
-            }
+            row = new RowConstraints(65);
+            col = new ColumnConstraints(65);
+
             grid.getRowConstraints().add(row);
             grid.getColumnConstraints().add(col);
         }
@@ -63,108 +58,108 @@ public class Viewer extends Application {
         //set up exits
         ImageView exitB0 = new ImageView();
         exitB0.setImage(new Image(Viewer.class.getResource(URI_BASE+"RailExit.png").toString()));
-        exitB0.setFitHeight(80);
-        exitB0.setFitWidth(80);
-        exitB0.setLayoutX(102);
-        exitB0.setLayoutY(170);
+        exitB0.setFitHeight(65);
+        exitB0.setFitWidth(65);
+        exitB0.setLayoutX(150-65);
+        exitB0.setLayoutY(100+65);
         exitB0.setRotate(270);
-        placementGroup.getChildren().add(exitB0);
+        root.getChildren().add(exitB0);
 
         ImageView exitD0 = new ImageView();
         exitD0.setImage(new Image(Viewer.class.getResource(URI_BASE+"HighExit.png").toString()));
-        exitD0.setFitHeight(80);
-        exitD0.setFitWidth(80);
-        exitD0.setLayoutX(102);
-        exitD0.setLayoutY(330);
+        exitD0.setFitHeight(65);
+        exitD0.setFitWidth(65);
+        exitD0.setLayoutX(150-65);
+        exitD0.setLayoutY(100+65*3);
         exitD0.setRotate(270);
-        placementGroup.getChildren().add(exitD0);
+        root.getChildren().add(exitD0);
 
         ImageView exitF0 = new ImageView();
         exitF0.setImage(new Image(Viewer.class.getResource(URI_BASE+"RailExit.png").toString()));
-        exitF0.setFitHeight(80);
-        exitF0.setFitWidth(80);
-        exitF0.setLayoutX(102);
-        exitF0.setLayoutY(490);
+        exitF0.setFitHeight(65);
+        exitF0.setFitWidth(65);
+        exitF0.setLayoutX(150-65);
+        exitF0.setLayoutY(100+65*5);
         exitF0.setRotate(270);
-        placementGroup.getChildren().add(exitF0);
+        root.getChildren().add(exitF0);
 
         ImageView exitB6 = new ImageView();
         exitB6.setImage(new Image(Viewer.class.getResource(URI_BASE+"RailExit.png").toString()));
-        exitB6.setFitHeight(80);
-        exitB6.setFitWidth(80);
-        exitB6.setLayoutX(102+600);
-        exitB6.setLayoutY(170);
+        exitB6.setFitHeight(65);
+        exitB6.setFitWidth(65);
+        exitB6.setLayoutX(150+65*7);
+        exitB6.setLayoutY(100+65);
         exitB6.setRotate(90);
-        placementGroup.getChildren().add(exitB6);
+        root.getChildren().add(exitB6);
 
         ImageView exitD6 = new ImageView();
         exitD6.setImage(new Image(Viewer.class.getResource(URI_BASE+"HighExit.png").toString()));
-        exitD6.setFitHeight(80);
-        exitD6.setFitWidth(80);
-        exitD6.setLayoutX(102+600);
-        exitD6.setLayoutY(330);
+        exitD6.setFitHeight(65);
+        exitD6.setFitWidth(65);
+        exitD6.setLayoutX(150+65*7);
+        exitD6.setLayoutY(100+65*3);
         exitD6.setRotate(90);
-        placementGroup.getChildren().add(exitD6);
+        root.getChildren().add(exitD6);
 
         ImageView exitF6 = new ImageView();
         exitF6.setImage(new Image(Viewer.class.getResource(URI_BASE+"RailExit.png").toString()));
-        exitF6.setFitHeight(80);
-        exitF6.setFitWidth(80);
-        exitF6.setLayoutX(102+600);
-        exitF6.setLayoutY(490);
+        exitF6.setFitHeight(65);
+        exitF6.setFitWidth(65);
+        exitF6.setLayoutX(150+65*7);
+        exitF6.setLayoutY(100+65*5);
         exitF6.setRotate(90);
-        placementGroup.getChildren().add(exitF6);
+        root.getChildren().add(exitF6);
 
         ImageView exitA1 = new ImageView();
         exitA1.setImage(new Image(Viewer.class.getResource(URI_BASE+"HighExit.png").toString()));
-        exitA1.setFitHeight(80);
-        exitA1.setFitWidth(80);
-        exitA1.setLayoutX(242);
-        exitA1.setLayoutY(30);
-        placementGroup.getChildren().add(exitA1);
+        exitA1.setFitHeight(65);
+        exitA1.setFitWidth(65);
+        exitA1.setLayoutX(150+65);
+        exitA1.setLayoutY(100-65);
+        root.getChildren().add(exitA1);
 
         ImageView exitA3 = new ImageView();
         exitA3.setImage(new Image(Viewer.class.getResource(URI_BASE+"RailExit.png").toString()));
-        exitA3.setFitHeight(80);
-        exitA3.setFitWidth(80);
-        exitA3.setLayoutX(402);
-        exitA3.setLayoutY(30);
-        placementGroup.getChildren().add(exitA3);
+        exitA3.setFitHeight(65);
+        exitA3.setFitWidth(65);
+        exitA3.setLayoutX(150+65*3);
+        exitA3.setLayoutY(100-65);
+        root.getChildren().add(exitA3);
 
         ImageView exitA5 = new ImageView();
         exitA5.setImage(new Image(Viewer.class.getResource(URI_BASE+"HighExit.png").toString()));
-        exitA5.setFitHeight(80);
-        exitA5.setFitWidth(80);
-        exitA5.setLayoutX(562);
-        exitA5.setLayoutY(30);
-        placementGroup.getChildren().add(exitA5);
+        exitA5.setFitHeight(65);
+        exitA5.setFitWidth(65);
+        exitA5.setLayoutX(150+65*5);
+        exitA5.setLayoutY(100-65);
+        root.getChildren().add(exitA5);
 
         ImageView exitG1 = new ImageView();
         exitG1.setImage(new Image(Viewer.class.getResource(URI_BASE+"HighExit.png").toString()));
         exitG1.setRotate(180);
-        exitG1.setFitHeight(80);
-        exitG1.setFitWidth(80);
-        exitG1.setLayoutX(242);
-        exitG1.setLayoutY(630);
-        placementGroup.getChildren().add(exitG1);
+        exitG1.setFitHeight(65);
+        exitG1.setFitWidth(65);
+        exitG1.setLayoutX(150+65);
+        exitG1.setLayoutY(100+65*7);
+        root.getChildren().add(exitG1);
 
         ImageView exitG3 = new ImageView();
         exitG3.setImage(new Image(Viewer.class.getResource(URI_BASE+"RailExit.png").toString()));
         exitG3.setRotate(180);
-        exitG3.setFitHeight(80);
-        exitG3.setFitWidth(80);
-        exitG3.setLayoutX(402);
-        exitG3.setLayoutY(630);
-        placementGroup.getChildren().add(exitG3);
+        exitG3.setFitHeight(65);
+        exitG3.setFitWidth(65);
+        exitG3.setLayoutX(150+65*3);
+        exitG3.setLayoutY(100+65*7);
+        root.getChildren().add(exitG3);
 
         ImageView exitG5 = new ImageView();
         exitG5.setImage(new Image(Viewer.class.getResource(URI_BASE+"HighExit.png").toString()));
         exitG5.setRotate(180);
-        exitG5.setFitHeight(80);
-        exitG5.setFitWidth(80);
-        exitG5.setLayoutX(562);
-        exitG5.setLayoutY(630);
-        placementGroup.getChildren().add(exitG5);
+        exitG5.setFitHeight(65);
+        exitG5.setFitWidth(65);
+        exitG5.setLayoutX(150+65*5);
+        exitG5.setLayoutY(100+65*7);
+        root.getChildren().add(exitG5);
     }
 
     /**
@@ -182,16 +177,16 @@ public class Viewer extends Application {
        for(int i=0; i<placement.length(); i+=5){
 
            String tile = placement.substring(i,i+2);
-           int rowPosition = (placement.substring(i+2,i+3).hashCode()-65)*80+90;
-           int colPosition = Integer.parseInt(placement.substring(i+3,i+4))*80+162;
+           int rowPosition = (placement.substring(i+2,i+3).hashCode()-65)*65+100;
+           int colPosition = Integer.parseInt(placement.substring(i+3,i+4))*65+150;
            int orientation = Integer.parseInt(placement.substring(i+4,i+5))*90;
 
            //set up tile
            img = new ImageView();
            img.setImage(new Image(Viewer.class.getResource(URI_BASE+tile+".png").toString()));
            img.setRotate(orientation);
-           img.setFitWidth(80);
-           img.setFitHeight(80);
+           img.setFitWidth(65);
+           img.setFitHeight(65);
            img.setPreserveRatio(true);
            img.setSmooth(true);
 
@@ -207,19 +202,30 @@ public class Viewer extends Application {
      * Create a basic text field for input and a refresh button.
      */
     private void makeControls() {
-        Label label1 = new Label("Placement:");
+
+        Label label1 = new Label("Placement String:");
         textField.setPrefWidth(100);
-        Button button = new Button("Refresh");
-        button.setOnAction(e -> {
+        textField.setPrefColumnCount(3);
+        Button button1 = new Button("Make Placement");
+        button1.setOnAction(e -> {
             makePlacement(textField.getText());
             textField.clear();
         });
+        Button button2 = new Button("Clear");
+        button2.setOnAction(e ->{
+            placementGroup.getChildren().clear();
+
+        });
+        button2.setLayoutX(880);
+        button2.setLayoutY(180);
         HBox hb = new HBox();
-        hb.getChildren().addAll(label1, textField, button);
+        hb.getChildren().addAll(label1, textField, button1);
         hb.setSpacing(10);
-        hb.setLayoutX(760);
-        hb.setLayoutY(VIEWER_HEIGHT - 500);
+        hb.setLayoutX(660);
+        hb.setLayoutY(150);
+
         controls.getChildren().add(hb);
+        controls.getChildren().add(button2);
     }
 
     @Override
