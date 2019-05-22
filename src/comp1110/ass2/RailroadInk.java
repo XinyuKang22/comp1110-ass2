c25271b8bb482690f213d844fbd8e878c0922e6d
Rebecca <u6598700@anu.edu.au>
2019-05-23 00:18:38 +1000

    "fixed task7"

--- src/comp1110/ass2/RailroadInk.java
+++ src/comp1110/ass2/RailroadInk.java
@@ -183,7 +183,7 @@ public class RailroadInk {
     public static void main(String[] args) {
         String a = "A1A30";
         String b = "A1A2A3B0";
-        System.out.println(generateMove(a,b));
+        System.out.println(generateDiceRoll());
     }
 
     /**
@@ -197,12 +197,11 @@ public class RailroadInk {
      * @return a String representing the die roll e.g. A0A4A3B2
      */
     public static String generateDiceRoll() {
-        int result, result1, result2, result3 = 0;
         Random r = new Random();
-        result = r.nextInt(5);
-        result1 = r.nextInt(5);
-        result2 = r.nextInt(5);
-        result3 = r.nextInt(2);
+        int result = r.nextInt(6);
+        int result1 = r.nextInt(6);
+        int result2 = r.nextInt(6);
+        int result3 = r.nextInt(3);
         String dice = "A" + Integer.toString(result) + "A" +  Integer.toString(result1) + "A" +  Integer.toString(result2) +"B" + Integer.toString(result3);
         return dice;
     }
